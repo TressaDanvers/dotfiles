5bc3b9e4f80e9da32bab99421401110e6ac492c7
Tressa Danvers <TDanvers@protonmail.ch>
2025-02-13 23:59:31 -0500

    removed default utilities folder app defaults

--- dconf.nix
+++ dconf.nix
@@ -4,7 +4,14 @@
     settings = {
       # Applications
       "org/gnome/shell".favorite-apps = [];
-      "org/gnome/desktop/app-folders".folder-children = [];
+      "org/gnome/desktop/app-folders/folders/Utilities".excluded-apps = [
+        "org.freedesktop.GnomeAbrt.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" 
+        "org.gnome.Connections.desktop" "org.gnome.Console.desktop" "org.gnome.tweaks.desktop" 
+        "org.gnome.DejaDup.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" 
+        "org.gnome.FileRoller.desktop" "org.gnome.font-viewer.desktop" "org.gnome.Loupe.desktop" 
+        "org.freedesktop.MalcontentControl.desktop" "org.gnome.seahorse.Application.desktop" 
+        "org.gnome.Usage.desktop"
+      ];
 
       # Shell Extensions
       "org/gnome/shell".disable-user-extensions = false;
