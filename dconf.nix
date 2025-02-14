138dabbe98471fd9b0d5498d8949f25ec426d1cf
Tressa Danvers <TDanvers@protonmail.ch>
2025-02-13 23:37:17 -0500

    enabled alphabetical appgrid extension

--- dconf.nix
+++ dconf.nix
@@ -5,6 +5,11 @@
       # Applications
       "org/gnome/shell".favorite-apps = [];
       "org/gnome/desktop/app-folders".folder-children = [];
+
+      # Shell Extensions
+      "org/gnome/shell".disable-user-extensions = false;
+      "org/gnome/shell".disabled-extensions = [];
+      "org/gnome/shell".enabled-extensions = [ "AlphabeticalAppGrid@stuarthayhurst" ];
     };
   };
 }
